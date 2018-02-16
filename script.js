194bfb283eed9d15b3a25c9734ed6cbe9d70f9c6
windshire <robert.g.mcfadden@gmail.com>
2018-02-15 17:18:43 -0700

    publish

--- script.js
+++ script.js
@@ -14,7 +14,30 @@ var clickPoke = function(e) {
             console.log(json);
             var data = "<h4>#" + json.id + ": " + json.name + " data:</h4>";
             data += "<img src=\""+json.sprites.front_default+"\">";
+
+            var delimiter = "<p>Type: ";
+
+            for (var i = 0; i < json.types.length; ++i) {
+                data += delimiter;
+                data += "<a class=\"" + json.types[i].type.name + "\">";
+                data += json.types[i].type.name;
+                data += "</a>";
+                delimiter = "/";
+            }
+
+            data += "</p>";
+
+            data += "<p>Height: " + json.height/10 + "m";
+            data += "</p>";
+
+            data += "<p>Weight: " + json.weight/10 + "kg";
+            data += "</p>";
+
+
             $("#pokeResult").html(data);
+        },
+        error: function() {
+            $("#pokeResult").html("QUERY FAILED (Did you input a valid id/name?)");
         }
     });
 }
